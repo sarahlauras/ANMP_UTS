7faa7405393ceb396c71f03192ecfff61ab388d9
Anastasya Putri <anastasyap41@gmail.com>
2025-10-03 12:57:23 +0700

    mvvm structure

--- app/src/main/java/com/mlbdev/anmp_miniproject/view/MainActivity.kt
+++ app/src/main/java/com/mlbdev/anmp_miniproject/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.mlbdev.anmp_miniproject
+package com.mlbdev.anmp_miniproject.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mlbdev.anmp_miniproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
